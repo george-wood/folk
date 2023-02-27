4e694a6e140118d9422a8915cef584ae23598d78
George Wood <george.wood@nyu.edu>
2023-02-27 16:15:40 +0000

    Initial commit

---
+++ R/acs.R
@@ -0,0 +1,54 @@
+get_data <- function(root_dir, year, horizon, survey,
+                     states = NULL, density = 1.0,
+                     random_seed = 0, join_household = FALSE,
+                     download = FALSE) {
+
+  data <- load_acs(
+    root_dir    = root_dir,
+    year        = year,
+    states      = states,
+    horizon     = horizon,
+    survey      = survey,
+    density     = density,
+    random_seed = random_seed,
+    download    = download
+  )
+
+  if (join_household) {
+
+    n_person <- nrow(data)
+
+    if (survey != "person") {
+      cli::cli_abort("`survey` must be 'person' to join household data.")
+    }
+
+    household_data <- load_acs(
+      root_dir = root_dir,
+      year     = year,
+      states   = states,
+      horizon  = horizon,
+      survey   = "household",
+      # serial_filter_list = list(data['SERIALNO']),
+      download = download
+    )
+
+    household_cols <- union(
+      setdiff(names(household_data), names(data)),
+      "SERIALNO"
+    )
+
+    data[household_data,
+         on = "SERIALNO",
+         (household_cols) := mget(paste0("i.", household_cols))]
+
+    if (nrow(data) != n_person) {
+      cli::cli_abort(
+        "Number of rows does not match after join: {nrow(data)} vs {n_person}"
+      )
+    }
+  }
+
+  data
+
+}
+
---
+++ README.md
@@ -0,0 +1,22 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# folk
+
+<!-- badges: start -->
+
+[![R-CMD-check](https://github.com/george-wood/folk/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/george-wood/folk/actions/workflows/R-CMD-check.yaml)
+[![codecov](https://codecov.io/gh/george-wood/folk/branch/master/graph/badge.svg?token=ZM8CUR8P13)](https://codecov.io/gh/george-wood/folk)
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+## Installation
+
+You can install the development version of folk from
+[GitHub](https://github.com/) using:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("george-wood/epts")
+```
